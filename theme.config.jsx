ff6b2aed592f3c903fd4d7f5136aa2c25af893c8
Evan <evansmalley2@gmail.com>
2023-09-12 21:10:34 -0400

    API docs

--- theme.config.jsx
+++ theme.config.jsx
@@ -3,7 +3,20 @@ export default {
   project: {
     link: 'https://github.com/esmalleydev/srating.io-docs'
   },
+  docsRepositoryBase: 'https://github.com/esmalleydev/srating.io-docs',
   sidebar: {
     defaultMenuCollapseLevel: 1
+  },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="srating API Docs" />
+      <meta property="og:description" content="srating API Documentation" />
+    </>
+  ),
+  footer: {
+    text: (
+      <span>SRATING LLC</span>
+    )
   }
 };
